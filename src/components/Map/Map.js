9be22d9835e9df876141b00dc80186c257d10a17
Renars Miljons <reku.miljons@gmail.com>
2022-10-26 22:23:41 +0300

    initialize location tracking on load

--- src/components/Map/Map.js
+++ src/components/Map/Map.js
@@ -3,9 +3,11 @@ import { useState } from "react";
 import { StyledMapContainer } from "./MapContainer.styled";
 /* eslint import/no-webpack-loader-syntax: off */
 import mapboxgl from "!mapbox-gl";
-import 'mapbox-gl/dist/mapbox-gl.css';
+import "mapbox-gl/dist/mapbox-gl.css";
+import { distance } from "../../utils/distance";
 
-mapboxgl.accessToken = 'pk.eyJ1Ijoic2llbmFvbmUiLCJhIjoiY2w5cHRpNmplMDJmYjNvbDdsY2ZxcWJobCJ9.IpoS9W6rp0EYf0XzYw-3ug';
+mapboxgl.accessToken =
+	"pk.eyJ1Ijoic2llbmFvbmUiLCJhIjoiY2w5cHRpNmplMDJmYjNvbDdsY2ZxcWJobCJ9.IpoS9W6rp0EYf0XzYw-3ug";
 
 export default function Map() {
 	const [darkMode, setDarkmode] = useState(false);
@@ -14,31 +16,82 @@ export default function Map() {
 	const [lng, setLng] = useState(24.1);
 	const [lat, setLat] = useState(56.94);
 	const [zoom, setZoom] = useState(13);
+	const [debug, setDebug] = useState(null);
+	const [returnData, setReturnData] = useState({
+		totalDistance: 0,
+		prevLng: lng,
+		prevLat: lat,
+		updateCount: 0,
+	});
+
+	function handleLocationChange(e) {
+		console.log("triggered");
+		setDebug(
+			<div id="return-data"
+			style={{
+				position: "fixed",
+				top: 400,
+				left: 0,
+				right: 0,
+				margin: "0 auto",
+				height: "100%",
+				backgroundColor: "rgba(0,0,0,0.4)",
+				display: "block",
+				color: "white",
+				textAlign: "center",
+			}}>
+				<p>previous latitude: {returnData.prevLat}</p>
+				<p>previous longitude: {returnData.prevLng}</p>
+				<p>latitude: {e.coords.latitude}</p>
+				<p>longitude: {e.coords.longitude}</p>
+				<p>total km: {returnData.totalDistance}</p>
+				<p>total m: {returnData.totalDistance*1000}</p>
+				<p>update count: {returnData.updateCount}</p>
+			</div>
+		);
+		setReturnData({
+			...returnData,
+			updateCount: returnData.updateCount + 1,
+			totalDistance: returnData.totalDistance + distance(returnData.prevLat, returnData.prevLng, e.coords.latitude, e.coords.longitude),
+			prevLat: e.coords.latitude,
+			prevLng: e.coords.longitude,
+
+		});
+	}
 
 	useEffect(() => {
 		if (map.current) return; // initialize map only once
 		map.current = new mapboxgl.Map({
-		container: mapContainer.current,
-		style: "mapbox://styles/mapbox/light-v10",
-		center: [lng, lat],
-		zoom: zoom
+			container: mapContainer.current,
+			style: "mapbox://styles/mapbox/light-v10",
+			center: [lng, lat],
+			zoom: zoom,
 		});
-		map.current.addControl(
-			new mapboxgl.GeolocateControl({
+		const geolocate = new mapboxgl.GeolocateControl({
 			positionOptions: {
-			enableHighAccuracy: true
+				enableHighAccuracy: true,
 			},
 			// When active, the map will receive updates to the device's location as it changes.
 			trackUserLocation: true,
 			// Draw an arrow next to the location dot to indicate which direction the device is heading.
-			showUserHeading: true
-			})
-			);
+			showUserHeading: true,
+			showAccuracyCircle: true,
 		});
+		map.current.addControl(geolocate);
+		map.current.on('load', () => {
+			geolocate.trigger();
+			});
+		geolocate.on("geolocate", (e) => handleLocationChange(e));
+
+		return () => {
+			geolocate.off("geolocate", (e) => handleLocationChange(e));
+		};
+	});
 
 	return (
-		<StyledMapContainer ref={mapContainer}>
-
-		</StyledMapContainer>
+		<>
+			<StyledMapContainer ref={mapContainer} />;
+			{debug}
+		</>
 	);
 }
